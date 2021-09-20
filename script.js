c0e8bc163648ba340a6e5185595599eca2a52367
De-Cay <dushik@live.com>
2021-09-20 22:19:52 +0530

    table cell used for game board

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
 const Game = {
   selectedVariant: '',
+  board: [Array.from(Array(3)), Array.from(Array(3)), Array.from(Array(3))],
+  tileList: ['1', '2' , '3', '4', '5', '6', '7', '8', '9']
 }
 
 printVariantAndCheckValid = (target) => {
   target.textContent = Game.selectedVariant;
 }
 
-const restartGame = ($dialog, $main, $tiles, tileList) => {
-  // Empty the game board
-  for (let ele of $tiles) {
-    ele.textContent = ''
-  }
-
+const restartGame = ($table, $dialog) => {
   // prompt the user to choose
   $dialog.show();
 
-  // Add Click event listener on the main
-  $main.addEventListener('click', (e) => {
-    const { target } = e;
-    const tileNum = target.getAttribute('data-tile');
-    if (tileList.includes(tileNum)) {
-      printVariantAndCheckValid(target);
-    }
-  }, false);
-  
   // Add Click event listener on the dilog
   $dialog.addEventListener('click', (e) => {
     const { target } = e;
@@ -33,15 +21,30 @@ const restartGame = ($dialog, $main, $tiles, tileList) => {
       Game.selectedVariant = selection;
     }
   }, false);
+
+  // Empty the game board
+  for(let row = 0, noOfRows = $table.rows.length; row < noOfRows; row++) {
+    const cells = $table.rows[row];
+    for (let cell = 0, noOfCells = cells.length; cell < noOfCells; cell++) {
+      cells[cell].textContent = '';
+    }
+  }
+  
+  // Add Click event listener on the main
+  $table.addEventListener('click', (e) => {
+    const { target } = e;
+    if (target.tagName === 'TD') {
+      console.log(target.parentNode.rowIndex, target.cellIndex);
+      printVariantAndCheckValid(target);
+    }
+  }, false);
 }
 
 const initializeGame = () => {
-  const $main = document.querySelector('main');
-  const $tiles = document.getElementsByClassName('tile');
+  const $table = document.querySelector('table');
   const $dialog = document.querySelector('dialog');
-  const tileList = ['1', '2' , '3', '4', '5', '6', '7', '8', '9'];
 
-  restartGame($dialog, $main, $tiles, tileList);
+  restartGame($table, $dialog);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
